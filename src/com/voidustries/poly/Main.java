299c5cff343eaf47a3d7aef3413929b0dde4d86d
Stone Monarch <joshleubecherrobinson@gmail.com>
2018-01-20 23:49:12 -0500

    things

---
+++ src/com/voidustries/poly/Main.java
@@ -0,0 +1,15 @@
+package com.voidustries.poly;
+
+// Class Created by Stone Monarch on 1/20/2018
+
+import java.util.logging.*;
+
+public class Main {
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    static {
+        LOGGER.setLevel(Level.FINE);
+    }
+
+}
