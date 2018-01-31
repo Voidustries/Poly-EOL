46b3ea86c5cb970e9ae24868a0a07e78b2a02ab9
Stone Monarch <joshleubecherrobinson@gmail.com>
2018-01-31 00:12:42 -0500

    Added Custom Formatter

---
+++ src/com/voidustries/poly/CustomFormatter.java
@@ -0,0 +1,37 @@
+package com.voidustries.poly;
+// Class Created by Stone Monarch on 1/30/2018
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+public class CustomFormatter extends java.util.logging.Formatter {
+
+    // Create a DateFormat to format the logger timestamp.
+    private static final DateFormat df = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss.SSS");
+
+    @Override
+    public String format(LogRecord record) {
+        StringBuilder builder = new StringBuilder(1000);
+        builder.append(df.format(new Date(record.getMillis()))).append(" - ");
+        builder.append("[").append(record.getLevel()).append("] - ");
+        builder.append("[").append(record.getSourceClassName()).append(".");
+        builder.append(record.getSourceMethodName()).append("] - ");
+        builder.append(formatMessage(record));
+        builder.append("\n");
+        return builder.toString();
+    }
+
+    @Override
+    public String getHead(Handler h) {
+        return super.getHead(h);
+    }
+
+    @Override
+    public String getTail(Handler h) {
+        return super.getTail(h);
+    }
+
+}
