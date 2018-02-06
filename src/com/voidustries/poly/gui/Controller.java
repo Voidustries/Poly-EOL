fd986df008361336a4cdab5b88d97a7c24d4127f
Stone Monarch <joshleubecherrobinson@gmail.com>
2018-02-05 22:44:23 -0500

    Save and, Load props

--- src/com/voidustries/poly/gui/Controller.java
+++ src/com/voidustries/poly/gui/Controller.java
@@ -11,7 +11,6 @@ Creator Comments:
 import com.voidustries.poly.Main;
 import javafx.fxml.FXML;
 import javafx.scene.control.Spinner;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.DirectoryChooser;
@@ -31,40 +30,22 @@ public class Controller {
     @FXML
     private TextField show_dir;
     @FXML
-    private Spinner rss_update;
+    private Spinner<Integer> rss_update;
     @FXML
-    private Spinner download_update;
+    private Spinner<Integer> download_update;
     @FXML
-    private Spinner f2_after;
-    @FXML
-    private TextArea logField;
+    private Spinner<Integer> f2_after;
 
     @FXML
     private void onApply(MouseEvent mouseEvent) {
         Main.LOGGER.info("Apply Clicked");
-        try {
-            Properties saveProps = new Properties();
-            saveProps.setProperty("rss_url", rss_url.getText());
-            saveProps.setProperty("database_dir", database_dir.getText());
-            saveProps.setProperty("show_dir", show_dir.getText());
-            saveProps.setProperty("rss_update", rss_update.getValue().toString());
-            saveProps.setProperty("download_update", download_update.getValue().toString());
-            saveProps.setProperty("f2", f2_after.getValue().toString());
-            saveProps.storeToXML(new FileOutputStream("settings.xml"), "");
-        } catch (IOException e) {
-            Main.LOGGER.warning("CANNOT SAVE TO FILE " + e);
-            logField.setText("WARNING: COULD NOT SAVE TO FILE");
-            return;
-        }
-        logField.appendText("RSS URL: " + rss_url.getText() + "\n");
-        logField.appendText("Update RSS every " + rss_update.getValue() + " hours" + "\n");
-        logField.appendText("Try downloads every " + download_update.getValue() + " hours" + "\n");
-        logField.appendText("Switch to second filter after " + f2_after.getValue() + " tries" + "\n");
+        saveSettings();
     }
 
     @FXML
     private void onCancel(MouseEvent mouseEvent) {
         Main.LOGGER.info("Cancel Clicked");
+        resetSettings();
         GUI.getMainStage().close();
     }
 
@@ -77,10 +58,8 @@ public class Controller {
             String dir = file.getAbsolutePath();
             database_dir.setText(dir);
             Main.LOGGER.info("Database DIR Set to '" + dir + "'");
-            logField.appendText("Database Directory: " + dir + "\n");
         } catch (NullPointerException e) {
             Main.LOGGER.warning("No Folder Chosen: " + e);
-            logField.appendText("Database Location: No Folder Chosen(" + e + ")\n");
         }
     }
 
@@ -93,28 +72,45 @@ public class Controller {
             String dir = file.getAbsolutePath();
             show_dir.setText(dir);
             Main.LOGGER.info("Show DIR Set to '" + dir + "'");
-            logField.appendText("Show Directory: " + dir + "\n");
         } catch (NullPointerException e) {
             Main.LOGGER.warning("No Folder Chosen: " + e);
-            logField.appendText("Show Directory: No Folder Chosen(" + e + ")\n");
         }
     }
 
     @FXML
-    private void setupSecondFilter(MouseEvent mouseEvent) {
+    private void setF2(MouseEvent mouseEvent) {
         Main.LOGGER.entering("FilterGUI", "Starting Filter GUI...");
 
         // TODO: Make Filter Setup GUI
     }
 
-    {
-        Properties loadProps = new Properties();
+    private void saveSettings() {
         try {
-            loadProps.loadFromXML(new FileInputStream("settings.xml"));
-
-            rss_url.setText(loadProps.getProperty("rss_url"));
+            Properties saveProps = new Properties();
+            saveProps.setProperty("rss_url", rss_url.getText());
+            saveProps.setProperty("database_dir", database_dir.getText());
+            saveProps.setProperty("show_dir", show_dir.getText());
+            saveProps.setProperty("rss_update", rss_update.getValue().toString());
+            saveProps.setProperty("download_update", download_update.getValue().toString());
+            saveProps.setProperty("f2", f2_after.getValue().toString());
+            saveProps.store(new FileOutputStream("settings.properties"), "");
         } catch (IOException e) {
-            Main.LOGGER.warning("CANNOT LOAD FILE " + e);
+            Main.LOGGER.warning("CANNOT SAVE TO FILE " + e);
+        }
+    }
+
+    private void resetSettings() {
+        try {
+            Properties resetProps = new Properties();
+            resetProps.load(new FileInputStream("settings.properties"));
+            rss_url.setText(resetProps.getProperty("rss_url"));
+            database_dir.setText(resetProps.getProperty("database_dir"));
+            show_dir.setText(resetProps.getProperty("show_dir"));
+            rss_update.getValueFactory().setValue(Integer.parseInt(resetProps.getProperty("rss_update")));
+            download_update.getValueFactory().setValue(Integer.parseInt(resetProps.getProperty("download_update")));
+            f2_after.getValueFactory().setValue(Integer.parseInt(resetProps.getProperty("f2")));
+        } catch (IOException e) {
+            Main.LOGGER.config("Reset failed: " + e);
         }
     }
 }
