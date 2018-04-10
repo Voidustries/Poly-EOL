fbfab363b126d88d2e81d71a5bcd5b16493eca41
Stone Monarch <joshleubecherrobinson@gmail.com>
2018-04-09 21:56:56 -0400

    Possible Fix

--- src/main/java/com/voidustries/poly/conection/tvdb/TVDB.java
+++ src/main/java/com/voidustries/poly/conection/tvdb/TVDB.java
@@ -4,18 +4,23 @@ import com.google.gson.JsonObject;
 import com.voidustries.poly.conection.AbstractAgent;
 import com.voidustries.poly.conection.HttpsRequestHandler;
 
+import java.io.IOException;
+
 public class TVDB extends AbstractAgent{
 
     private final String api_url = "https://api.thetvdb.com";
+    private JsonObject token;
 
 
     public TVDB(String api) {
         setApi(api);
+        auth();
     }
 
     public TVDB(String api, String lang, String country) {
         setApi(api);
         setLocale(lang, country);
+        auth();
     }
 
     private void auth(){
@@ -28,6 +33,11 @@ public class TVDB extends AbstractAgent{
 
         body.addProperty("apikey", getApi());
 
-        setToken(HttpsRequestHandler.post(api_url, "/login", body, head));
+        try {
+            token = HttpsRequestHandler.post(api_url, "/login", body, head);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        setToken(token);
     }
 }
