ea02d8f02bf85afeeddb19b2b2cc1ed8a7f9632b
joshl <joshleubecherrobinson@gmail.com>
2018-04-17 19:02:34 -0400

    Added Documentation for future JavaDoc

--- src/main/java/com/voidustries/poly/conection/tvdb/TVDB.java
+++ src/main/java/com/voidustries/poly/conection/tvdb/TVDB.java
@@ -6,23 +6,50 @@ import com.voidustries.poly.conection.HttpRequestHandler;
 
 import java.io.IOException;
 
+/**
+ * The Poly implementation of TheTVDB API
+ */
 public class TVDB extends AbstractAgent{
 
+    /**
+     * API URL for TheTVDB
+     */
     private final String api_url = "https://api.thetvdb.com";
+
+    /**
+     * Token used for all requests after initial authentication
+     */
     private JsonObject token;
 
+    /**
+     * Token to be added to the header of requests under the "Authorization" key
+     */
+    private String headerToken;
 
+    /**
+     * Constructor for TheTVDB
+     * @param api your API key for TheTVDB
+     */
     public TVDB(String api) {
         setApi(api);
         auth();
     }
 
+    /**
+     * Constructor for TheTVDB
+     * @param api your API key for TheTVDB
+     * @param lang Preferred language
+     * @param country Preferred Country
+     */
     public TVDB(String api, String lang, String country) {
         setApi(api);
         setLocale(lang, country);
         auth();
     }
 
+    /**
+     * Authenticates the object with TheTVDB
+     */
     private void auth(){
 
         final JsonObject head = new JsonObject();
@@ -39,5 +66,6 @@ public class TVDB extends AbstractAgent{
             e.printStackTrace();
         }
         setToken(token);
+        headerToken = "Bearer " + getToken();
     }
 }
